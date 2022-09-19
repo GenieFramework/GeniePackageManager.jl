c884bed14da0d32d3067b2fd32fe88230c729822
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2022-09-19 20:12:46 +0530

    initial commit

---
+++ src/GeniePackageManager.jl
@@ -0,0 +1,14 @@
+module GeniePackageManager
+
+using Genie
+
+function install(dest::String; force = false)
+  src = abspath(normpath(joinpath(@__DIR__, "..", Genie.Plugins.FILES_FOLDER)))
+
+  for f in readdir(src)
+    isdir(f) || continue
+    Genie.Plugins.install(joinpath(src, f), dest, force = force)
+  end
+end
+
+end # module GeniePackageManager
