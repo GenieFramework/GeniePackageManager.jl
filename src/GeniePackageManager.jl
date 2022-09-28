ead267f253ef9ed787e0b37aae03f166125ae9e1
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2022-09-28 14:58:59 +0530

    improvement in API and adding vue frontend

--- src/GeniePackageManager.jl
+++ src/GeniePackageManager.jl
@@ -3,8 +3,12 @@ module GeniePackageManager
 using Genie
 using GeniePlugins
 using Genie.Renderer.Json
+using Genie.HTTPUtils.HTTP
 using Pkg
 using TOML
+using Random
+
+const PKGLISTURL = "https://raw.githubusercontent.com/JuliaRegistries/General/master/Registry.toml"
 
 function install(dest::String; force = false)
   src = abspath(normpath(joinpath(@__DIR__, "..", GeniePlugins.FILES_FOLDER)))
@@ -26,14 +30,72 @@ function list_packages()
   return installs |> json
 end
 
-function add()
-  package = params(:package)
-  Pkg.add(package)
+function get_packages() :: Dict{String,Any}
+  try
+    withcache(randstring(12), 24 * 60 * 60) do
+      Dict("packages" => 
+          [package_info["name"] for package_info in values(((HTTP.get(PKGLISTURL).body |> String) |> TOML.parse)["packages"])] 
+      ) |> json
+    end
+  catch e
+    return Dict("error" => e) |> json
+  end
 end
+
+module API
+
+module V1
+
+function add()
+  try
+    package = params(:package)
+    Pkg.add(package)
+    return Dict(:status => "ok", :message => "Package $package added") |> json
+  catch e
+    return Dict("error" => e) |> json
+  end
+end
+
+# function add_with_version()
+#   try
+#     package = params(:package)
+#     version = params(:version)
+#     Pkg.add(name=package, version=version)
+#     return Dict(:status => "ok", :message => "Package $package@$version added") |> json
+#   catch e
+#     return Dict("error" => e) |> json
+#   end
+# end
 
 function remove_package()
-  package = params(:package)
-  Pkg.rm(package)
+  try
+    package = params(:package)
+    Pkg.rm(package)
+    return Dict(:status => "ok", :message => "Package $package removed") |> json
+  catch e
+    return Dict("error" => e) |> json
+  end
 end
 
+function update_package()
+  try
+    package = params(:package)
+    Pkg.update(package)
+    return Dict(:status => "ok", :message => "Package $package updated") |> json
+  catch e
+    return Dict("error" => e) |> json
+  end
+end
+
+# function update_all_packages()
+#   try
+#     Pkg.update()
+#     return Dict(:status => "ok", :message => "All packages updated") |> json
+#   catch e
+#     return Dict("error" => e) |> json
+#   end
+# end
+
+end # module V1
+end # module API
 end # module GeniePackageManager
